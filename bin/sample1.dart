faeceefec2fa3972756179abe87c16e5b8ec2464
favazz-10 <favaz2001@gmail.com>
2022-09-19 15:58:57 +0530

    commit 2

---
+++ bin/sample1.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+void main(){
+  String name = 'favaz';
+  int age = 21;
+  double mark = 8.5;
+  String email = "favaz2001@gmail.com";
+  int ph = 8078081609;
+  int a=60;
+  int b=70;
+  String s='sum';
+  String d='difference';
+  String m="multiplication";
+  String D='division';
+
+
+  print("my name is $name");
+  print('iam $age years old');
+  print('my email id is $email');
+  print('my mark is $mark');
+  print("$ph is my ph number");
+  stdout.write('this is my first dart project');
+  print('\n$s=${a+b}');
+  print('$d=${a-b}');
+  print('$m=${a*b}');
+  print('$D=${a/b}');
+
+
+}
